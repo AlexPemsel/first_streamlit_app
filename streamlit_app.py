0f048ab21a3373aea762247ac11c45e16b3948c5
AlexPemsel <137157794+AlexPemsel@users.noreply.github.com>
2023-06-20 11:20:02 +0200

    Update streamlit_app.py
    Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents new healty diner')
+streamlit.title('My parents new healty diner')
+
+streamlit.header('New breakfast menu:')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Fruit salad')
+streamlit.text('Hard-Boiled egg')
